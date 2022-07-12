c2c2475943f66eca1cdeb856c1f359eaaaedfac6
aiisha-ali <107883624+aiisha-ali@users.noreply.github.com>
2022-07-12 09:51:07 -0500

    Add files via upload

---
+++ assessment-front-end-web 2/profile.js
@@ -0,0 +1,17 @@
+let favoriteColor = document.querySelector("#color")
+let favoritePlace = document.querySelector("#place")
+let favoriteRitual = document.querySelector("#ritual")
+
+function bestColor(){
+    alert("Pink")
+}
+favoriteColor.addEventListener("click", bestColor)
+
+function bestPlace(){
+    alert("Turkey")
+}
+favoritePlace.addEventListener("click", bestPlace)
+
+function bestRitual(){
+    alert("Netflix Binging")
+}
